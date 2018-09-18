9e6e587692cb158161d135e5d1b3b64e86e6817a
Progoogler <andyzanli@gmail.com>
2018-09-18 12:42:57 -0700

    Added Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { colors, fonts } from '../../config/styles';
+import Eicon from 'react-native-vector-icons/Entypo'
+
+export default class Header extends React.PureComponent {
+  constructor() {
+    super();
+    this.handleMenu = this.handleMenu.bind(this);
+  }
+  render() {
+    // const { navigation } = this.props;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Think Habit</Text>
+        <TouchableOpacity
+          activeOpacity={.8}
+          onPress={this.handleMenu}
+        >
+          <Eicon name={'menu'} style={styles.menuIcon} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  handleMenu() {
+    this.props.navigation.openDrawer();
+  }
+}
+
+import { ScaledSheet } from 'react-native-size-matters';
+
+const styles = ScaledSheet.create({
+  container: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    borderBottomWidth: 1,
+    borderColor: colors.darkGrey,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: '65@ms',
+    paddingHorizontal: '15@ms',
+  },
+  menuIcon: {
+    color: colors.darkGrey,
+    fontSize: fonts.large,
+  },
+  title: {
+    color: colors.primary,
+    fontSize: fonts.large,
+  },
+})
