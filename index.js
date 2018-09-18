d512f1c1bb7d0ceb3886acd817f41197aa71bcde
Progoogler <andyzanli@gmail.com>
2018-09-17 17:32:23 -0700

    Disable yellowbox warnings

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
+YellowBox.ignoreWarnings([]);
+YellowBox.ignoreWarnings(
+  [
+    'Warning: isMounted(...) is deprecated', 
+    'Module RCTImageLoader',
+  ]
+)
 AppRegistry.registerComponent('ThinkHabit', () => App);
