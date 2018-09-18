f41a89bb05a8c210c9f08ee34e10dc0b52e9dbc5
Progoogler <andyzanli@gmail.com>
2018-09-18 11:31:57 -0700

    Added timeout delay and splash theme

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -2,22 +2,61 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
-// import styles from './styles';
+import { colors } from '../../config/styles';
+import Micon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NavigationActions, StackActions } from 'react-navigation';
 
 export default class SplashScreen extends React.PureComponent {
   render() {
     return (
       <View style={styles.container}>
-
+        <Micon name={'brain'} style={styles.brain} />
       </View>
     )
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.appReady !== nextProps.appReady) {
+      const appSet = nextProps.appSet;
+      setTimeout(() => {
+        // if (appSet) {
+        if (!appSet) {
+          const resetAction = StackActions.reset({
+            index: 0,
+            actions: [
+              NavigationActions.navigate({
+                routeName: 'AppNavigator',
+              })
+            ]
+          });
+          this.props.navigation.dispatch(resetAction);
+        } else {
+          const resetAction = StackActions.reset({
+            index: 0,
+            actions: [
+              NavigationActions.navigate({
+                routeName: 'Selection',
+              })
+            ]
+          });
+          this.props.navigation.dispatch(resetAction);
+        }
+      }, 1500);
+    }
   }
 }
 
 import { ScaledSheet } from 'react-native-size-matters';
 
 const styles = ScaledSheet.create({
+  brain: {
+    color: '#FFFFFF',
+    fontSize: '75@ms',
+  },
   container: {
+    alignItems: 'center',
+    backgroundColor: colors.primary,
     flex: 1,
+    justifyContent: 'center',
   }
-})
+});
