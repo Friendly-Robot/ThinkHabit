f7bd09b37fc42d31e9444357b6f7c582bed7c477
Progoogler <andyzanli@gmail.com>
2018-09-18 11:32:10 -0700

    Add primary color and fonts

---
+++ src/config/styles.js
@@ -0,0 +1,16 @@
+import { moderateScale } from 'react-native-size-matters';
+
+export const colors = {
+  primary: '#1568A1',
+  darkGrey: '#545454',
+  grey: '#d3d3d3',
+  lightGrey: '#FAFAFA',
+}
+
+export const fonts = {
+  xs: moderateScale(12, 0.2),
+  small: moderateScale(14, 0.1),
+  medium: moderateScale(18, 0.1),
+  large: moderateScale(22, 0.2),
+  xl: moderateScale(32),
+};
