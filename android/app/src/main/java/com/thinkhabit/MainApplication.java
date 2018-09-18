d29d8dd6194fc1806008c80f761b189ac5dfa89c
Progoogler <andyzanli@gmail.com>
2018-09-17 21:55:48 -0700

    Added realm and fixed RNPN

--- android/app/src/main/java/com/thinkhabit/MainApplication.java
+++ android/app/src/main/java/com/thinkhabit/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactNativePushNotificationPackage(),
-            new ReactMaterialKitPackage()
+            new ReactMaterialKitPackage(),
+            new RealmReactPackage()
       );
     }
 
