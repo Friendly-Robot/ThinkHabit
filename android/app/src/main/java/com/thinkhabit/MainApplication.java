30b70ef1edfff09205217b4c572c21ee5116dab3
Progoogler <andyzanli@gmail.com>
2018-09-19 13:40:44 -0700

    Added RN Autogrow TextInput lib

--- android/app/src/main/java/com/thinkhabit/MainApplication.java
+++ android/app/src/main/java/com/thinkhabit/MainApplication.java
@@ -3,6 +3,7 @@ package com.thinkhabit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.autogrowtextinput.AutoGrowTextInputPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AutoGrowTextInputPackage(),
             new VectorIconsPackage(),
             new ReactNativePushNotificationPackage(),
             new ReactMaterialKitPackage(),
