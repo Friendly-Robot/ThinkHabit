fd0a988f2114d0df564bc2e15b2228bf4b3aae50
Progoogler <andyzanli@gmail.com>
2018-09-19 23:24:30 -0700

    Prevent Text fontscaling

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { AppRegistry, YellowBox } from 'react-native';
+import { AppRegistry, Text, YellowBox } from 'react-native';
 import App from './src/App';
+Text.defaultProps.allowFontScaling = false;
 YellowBox.ignoreWarnings([]);
 YellowBox.ignoreWarnings(
   [
     'Warning: isMounted(...) is deprecated', 
     'Module RCTImageLoader',
+
+    'Warning: Class RCTCxxModule was not exported', 
+    'Module RCTCxxModule',
+    'Class RCTCxxModule',
   ]
-)
+);
 AppRegistry.registerComponent('ThinkHabit', () => App);
