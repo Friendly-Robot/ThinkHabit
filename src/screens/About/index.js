098932e99271e557599598c108fc7f28e976b029
Progoogler <andyzanli@gmail.com>
2018-09-22 00:40:02 -0700

    Include Header in About

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
-// import styles from './styles';
+import Header from '../../components/Header';
 
 export default class About extends React.PureComponent {
   render() {
+    const { 
+      navigation 
+    } = this.props;
+
     return (
       <View style={styles.container}>
-
+        <Header navigation={navigation} />
       </View>
     )
   }
